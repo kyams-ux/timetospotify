0c8b268092a1a3f9872e91e5c25a773754e12895
Kayla <kyamadaa@hmc.edu>
2020-09-11 09:20:09 -0700

    spotify auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
+import SpotifyWebApi from 'spotify-web-api-js'
+//import '../../spotify-web-api-js-master/spotify-web-api-js-master/src/spotify-web-api.js';
+
+const client_id = "639db5cc638f46aeb796d411a8f4de45";
+const client_secret = "b392e37deb804c08a6e56e613b9e3884";
+const redirect_uri = "http://timetospotify.vercel.app";
+const id = "43ZHCT0cAZBISjO8DG9PnE";
+const auth = "BQAw_fIvgFUpFFTf2AoRzEdq8H0lHXTMpCZvYuF-mr4DNJPkT2KImnjvkECj3cqzxgKbcyYLtRJoli8w-51F0kTcrWRy0_xfDzL9zy2KJ8m853MXP22i7L6DNG9dRZcaZpOs-kOalwt44w";
 
 function App() {
+  var Spotify = require('spotify-web-api-js');
+  var s = new Spotify();
+  var spotifyApi = new SpotifyWebApi();
+  spotifyApi.setAccessToken(auth);
+  spotifyApi.searchTracks('Love').then(
+    function (data) {
+      console.log('Search by "Love"', data);
+    },
+    function (err) {
+      console.error(err);
+    }
+  );
+
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
