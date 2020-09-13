81e0744f9dc51849802d1b9f61cc441744a94fab
Kayla <kyamadaa@hmc.edu>
2020-09-13 15:38:34 -0700

    genre issue

--- src/App.js
+++ src/App.js
@@ -13,14 +13,18 @@ function App() {
   const client_secret = "b392e37deb804c08a6e56e613b9e3884";
   const redirect_uri = "http://timetospotify.vercel.app";
   const id = "43ZHCT0cAZBISjO8DG9PnE";
-  const accessToken = "BQAazLJSkSGGVcJMX0I8cMjDNjFKxd-VRv8m0NZ24VtTtRvTyoHQ9zKP8TCoTT2ybIQLqZAtPWVqQNTWFKlJuqedq9C1EoQ-f8Oc6cFJOILbHTnoByCbIYesQICa_tGc_G4pN7Kn3cnCqg";
-  const duration = "8";
+  const accessToken = "BQAdGhYP_PxAPwHktDFz9HLrtWhwcADQR4GgQ7_WWuF6Rwq-bz2qHBGBpmtCDHRuH-ZiZ6_H5HHOqyzVO1XNTxy02R0VRDQXpRUn62qmU7mTp73Sh0UMTfWy7iR5Nsu0tCCqSACrngmThQ";
+  const duration = "9";
 
-  const [items, setItems] = useState([]);
-  const [genre, setGenre] = useState('');
+  const [items, setItems] = useState('');
+  const [items2, setItems2] = useState('');
+  const [items3, setItems3] = useState('');
+  const [items4, setItems4] = useState('');
+  const [dur0, setDur0] = useState(0);
+  const [genre, setGenre] = useState("pop");
 
   useEffect(() => {
-    fetch("https://api.spotify.com/v1/search?q=genre:pop&type=track",{
+    fetch('https://api.spotify.com/v1/search?q=genre:'+genre+'&type=track',{
             method: 'GET', headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
@@ -28,9 +32,14 @@ function App() {
             }
         })
             .then(res => res.json())
-            .then((result) => {
-              setItems(result.tracks[0].items.name);
-            })
+            .then(results => {setItems(results.tracks.items[0].name)
+                             setItems2(results.tracks.items[1].name)
+                             setItems3(results.tracks.items[2].name)
+                             setItems4(results.tracks.items[3].name)
+                             setDur0(results.tracks.items[0].duration_ms)})
+            .then(console.log(items))
+              .then(result => console.log(result.tracks.items[0].name) )
+            .then(console.log('items: 1'))
         },[])
   //var SpotifyWebApi = require('spotify-web-api-js');
   //var spotifyApi = new SpotifyWebApi();
@@ -68,20 +77,20 @@ const handleSubmit = (event) => {
     <main>
       <h1>Time to Spotify</h1>
       <h2>A timer using the lengths of songs</h2>
-      <p>Track 1: {duration}</p>
-     <form>
-       <label>
-       Genre:
-       <select onChange={handleChange}>
-          <option value="pop">Pop</option>
-          <option value="edm">EDM</option>
-          <option selected value="hip%20hop">Hip Hop</option>
-          <option value="latin">Latin</option>
-          </select>
-       </label>
-       <input type="submit" value="Submit" />
-     </form>
-      <p>Track name: {items}</p>
+      <p>Choose a genre:</p>
+      <select onChange = {handleChange}>
+        <option value="pop">Pop</option>
+        <option value="hip%20hip">Hip Hop</option>
+        <option value="classical">Classical</option>
+        <option value="EDM">EDM</option>
+      </select>
+      <h2> Results </h2>
+      <p>Track 1 name: {items} </p>
+      <p>Track 1 duration: {Math.floor(dur0/60000)} min {parseFloat((dur0/60000 %1)*60).toFixed(1)} sec</p>
+      <br />
+      <p>Track 2 name: {items2} </p>
+      <p>Track 3 name: {items3} </p>
+      <p>Track 4 name: {items4} </p>
       <p>Genre: {genre}</p>
       <br />
       <h2>The date according to Go is:</h2>
